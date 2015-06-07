88dcdec2b395ed8548b5b02133b236551765f503
crinner <crinner@users.noreply.github.com>
2015-06-07 18:50:45 -0400

    Update dem_gen.py

--- dem_gen.py
+++ dem_gen.py
@@ -3,7 +3,7 @@ from pymclevel import alphaMaterials
 
 """
 Script for importing digital elevation model (DEM) into Minecraft/McEdit
-June 2015 by Thunderstage
+June 2015 by Claus Rinner
 
 Partially based on jsa's "mountain_gen.py" - generating hilly & mountainous terrain in MCEdit.
 Uses ASCII GRID import according to http://geospatialpython.com/2013/12/python-and-elevation-data-ascii-grid.html
